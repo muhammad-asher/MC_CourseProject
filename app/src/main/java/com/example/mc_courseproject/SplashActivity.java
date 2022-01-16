53a9911cf6c21d1cac3f9cb47e5395ffd681dd02
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2022-01-16 20:01:58 +0500

    Splash Activity Done

---
+++ app/src/main/java/com/example/mc_courseproject/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.mc_courseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler h=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },1500);
+
+
+
+    }
+
+}
