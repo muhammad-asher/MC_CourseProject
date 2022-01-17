9ffac9a37a840e1741e7c6ccef4bbfad3a6370aa
muhammad-asher <70090506+muhammad-asher@users.noreply.github.com>
2022-01-17 22:27:42 +0500

    Create view to add notes

--- app/src/main/java/com/example/mc_courseproject/MainActivity.java
+++ app/src/main/java/com/example/mc_courseproject/MainActivity.java
@@ -1,14 +1,78 @@
 package com.example.mc_courseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.mc_courseproject.Adapter;
+import com.example.mc_courseproject.AddNotesActivity;
+import com.example.mc_courseproject.Model;
+import com.example.mc_courseproject.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
+    Adapter adapter;
+    List<Model> notesList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recycler_view);
+        fab=findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, AddNotesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        notesList=new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter=new Adapter(this,MainActivity.this,notesList);
+        recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.searchbar);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setQueryHint("Search Notes Here");
+        
+        SearchView.OnQueryTextListener listener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        };
+
+        searchView.setOnQueryTextListener(listener);
+
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
